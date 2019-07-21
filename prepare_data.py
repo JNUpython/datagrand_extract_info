bffff2f85fb66ce24e937dddba1624308c041564
kaen <keanleelfm@gmail.com>
2019-07-21 09:52:52 +0800

    数据预处理train存在bug

--- prepare_data.py
+++ prepare_data.py
@@ -15,12 +15,17 @@ from gensim.models import Word2Vec
 from my_log import logger
 from tqdm import tqdm
 import time
-
+from lstm_crf.utils import config
+import pickle
+import numpy as np
+from functools import reduce
+from collections import defaultdict
 
 # 分词词汇的正则
 patt = re.compile("[\n_\s]+")
 
-def word_count(files, filter_min=1):
+
+def word_count(files, filter_min=1, save=True):
     """
     统计词频，对于一些低频的字符什么都学不到
     :param files:
@@ -30,6 +35,7 @@ def word_count(files, filter_min=1):
     words = []
     tags = []
     for file in files:
+        logger.info(file)
         with open(file, encoding="utf-8", mode="r") as file:
             # print(file.read())
             string = file.read()
@@ -52,8 +58,13 @@ def word_count(files, filter_min=1):
             save_words.add(k)
         else:
             filter_words.add(k)
-    logger.info("count for all(%d) save(%d) filter(%d)" % (len(counter), len(filter_words), len(save_words)))
+    logger.info("count for all(%d) save(%d) filter(%d)" % (len(counter), len(save_words), len(filter_words)))
     # print(counter)
+    if save:
+        with open("data/words.pkl", mode="wb") as file:
+            pickle.dump(save_words, file)
+            file.close()
+            logger.info("save words")
     return save_words, filter_words
 
 
@@ -73,8 +84,8 @@ def word2vec(model_path, corpus, embeding_size=256, min_count=1, window=7):
             file.write(word + " " + " ".join(vector) + "\n")
 
 
-def run_word2vec(files, unk_words, window):
-    f = lambda x: x if x not in unk_words else "unk"
+def run_word2vec(files, words, window):
+    f = lambda x: x if x in words else config["unknow_word"]
     corpus = []
     for file in files:
         with open(file, encoding="utf-8", mode="r") as file:
@@ -110,8 +121,8 @@ def piece2tag(string):
     return result
 
 
-def prepare_data_lstm_crf(file, data_type, unk_words=set()):
-    f = lambda x: x if x not in unk_words else "unk"
+def prepare_data_lstm_crf(file, data_type):
+    logger.info(file)
     assert data_type in ["train", "test", "dev"]
     file = open(file, encoding="utf-8", mode="r")
     input_seq = []
@@ -123,20 +134,37 @@ def prepare_data_lstm_crf(file, data_type, unk_words=set()):
 
     else:
         for line in tqdm(file.readlines()):
+            # for line in file.readlines():
+            # logger.info(line)
             # train data 的piece 通过空格分割
             pieces = re.split("\s+", line.strip())
-            line_input_seq = []
-            line_output_seq = []
-            for piece_res in map(piece2tag, pieces):
+            # logger.info(pieces)
+            line_seq = []
+            line_tag = []
+            pieces_res = list(map(piece2tag, pieces))
+            for piece_res in pieces_res:
+                # logger.info(piece_res)
                 for item1, item2 in piece_res:
-                    line_input_seq.append(item1)
-
-                    line_output_seq.append(item2)
-            input_seq.append(line_input_seq)
-            output_seq.append(line_output_seq)
+                    line_seq.append(item1)
+                    # logger.info(item1)
+                    # logger.info(line_seq)
+                    line_tag.append(item2)
+            input_seq.append(line_seq)
+            output_seq.append(line_tag)
+            # logger.info(line_seq)
+            # logger.info(line_tag)
+            # print()
     file.close()
     # mask unk
-    input_seq = [list(map(f, line)) for line in input_seq]
+    logger.info(input_seq[:4])
+
+    def f1(x, y):
+        return x + y
+
+    input_seq_concat = list(reduce(f1, input_seq))
+    logger.info(input_seq_concat[:500])
+    logger.info("输入的词汇丰富程度%s： %d" % (data_type, len(set(input_seq_concat))))
+
     file = open("data/lstm_crf/%s.txt" % data_type, encoding="utf-8", mode="w")
     file_lines = ["\n".join(words) for words in input_seq]
     if data_type != "test":
@@ -146,17 +174,61 @@ def prepare_data_lstm_crf(file, data_type, unk_words=set()):
             assert len(line_seqs) == len(line_tags)
             pair = [item1 + "\t" + item2 for item1, item2 in zip(line_seqs, line_tags)]
             file_lines.append("\n".join(pair))
+
     file.write("\n\n".join(file_lines))
     logger.info("%s sentences  num: %d" % (data_type, len(file_lines)))
 
 
+def get_voc_dict(files):
+    """
+    统计词频，对于一些低频的字符什么都学不到
+    :param files:
+    :param filter_min:
+    :return:
+    """
+    words = []
+    tags = []
+    for file in files:
+        logger.info(file)
+        with open(file, encoding="utf-8", mode="r") as file:
+            # print(file.read())
+            string = file.read()
+            # 用到train data
+            words.extend([w.split("/")[0] for w in patt.split(string.strip())])
+            del string
+    counter = Counter(words)
+    logger.info(counter)
+    word_ids = defaultdict()
+    for index, word in enumerate(counter.keys()):
+        word_ids[word] = index
+    logger.info(word_ids)
+    logger.info(len(word_ids))
+    pickle.dump(word_ids, open("data/train_test_word2ids.pkl", mode="wb"))
+    return word_ids
+
+
 if __name__ == '__main__':
-    files = ["data/corpus.txt", "data/train.txt"]
-    save_words, filter_words = word_count(files, 2)
+    # 统计train 和test的词频
+    files = ["data/train.txt"]
+    word_count(files, 1, False)
+    files = ["data/test.txt"]
+    word_count(files, 1, False)
+    # 获取train和test的词汇
+    files = ["data/train.txt", "data/test.txt"]
+    word2ids = get_voc_dict(files)
+    logger.info("需要的voc  size 大小 %d" % len(word2ids))
+
+
+    # files = ["data/corpus.txt", "data/train.txt"]
+    # save_words, filter_words = word_count(files, 2)
+    # save_words = pickle.load(open("data/words.pkl", mode="rb"))
+
     # print(piece2tag("12266/c"))
     # print(piece2tag("17488_12266/c"))
     # print(piece2tag("17488_19311_12266/c"))
-    prepare_data_lstm_crf("data/test.txt", "test", filter_words)
-    prepare_data_lstm_crf("data/train.txt", "train", filter_words)
-    files = ["data/corpus.txt", "data/train.txt", "data/test.txt"]
-    run_word2vec(files, unk_words=filter_words, window=6)
+
+    # 将train和test准备成标准的输入模式
+    prepare_data_lstm_crf("data/test.txt", "test")
+    prepare_data_lstm_crf("data/train.txt", "train")
+    # files = ["data/corpus.txt", "data/train.txt", "data/test.txt"]
+    # run_word2vec(files, unk_words=filter_words, window=6)
